f1b1207d50b4ad9266279c1bae67883dfc09ea51
J. Livsey <james.a.livsey@gmail.com>
2020-11-04 06:19:15 -0500

    upd data-setup

--- tests/1-B12/ire/data-setup.R
+++ tests/1-B12/ire/data-setup.R
@@ -1,18 +1,27 @@
 # ---- Simulate Data ----------------------------------------------------------
 
-nreps <- 200
+nreps <- 4
 dataList <- vector(mode = "list", length = nreps)
 
 for(i in 1:nreps){
 
   set.seed(i)
 
-  N = 3
-  T <- TT <- 300
-  t = 1:T
-  Phi=diag(N)
-  Sig=diag(N); Sig[1,2] <- Sig[2,1] <- .75
+  # ---- Input parameters ----
+  N  <- 3
+  T  <- 300
+  TT <- T   # I use TT and Tucker uses T
+  t  <- 1:T
+  start.date <- c(1990, 1) # generic
+  period     <- 12
+  transform  <- "none"
+  aggregate  <- FALSE
+  subseries  <- 1:3
 
+  Phi <- diag(N)
+  Sig <- diag(N); Sig[1,2] <- Sig[2,1] <- .75
+
+  # ---- generate structural components
   s1 = gen_trendComp(T, Phi, Sig)
   s2 = gen_seasComp(T, Phi, diag(N))
   s0 = rmvnorm(n = T, mean = rep(0,N), sigma = diag(N))
@@ -20,21 +29,14 @@ for(i in 1:nreps){
   data = s1+s2+s0
   data = demean(data)
 
-  # plot(ts(data), main="simulated series")
-
-  # Load
-  start.date <- c(1990, 1)
-  period <- 12
+  # ---- Load ----
   dataALL.ts <- sigex.load(data = data,
                            start.date = start.date,
                            period = period,
-                           epithets = c("Dim1","Dim2","Dim3"),
+                           epithets = c("Dim1", "Dim2", "Dim3"),
                            plot = FALSE)
 
-  # Prep for sigex
-  transform <- "none"
-  aggregate <- FALSE
-  subseries <- c(1,2,3)
+  # ---- Prep for sigex ----
   begin.date <- start.date
   end.date <- end(dataALL.ts)
   range <- list(begin.date,end.date)
@@ -45,6 +47,7 @@ for(i in 1:nreps){
                         range = range,
                         plot = FALSE)
 
+  # ---- input data to List ----
   dataList[[i]] <- data.ts
 
 }
